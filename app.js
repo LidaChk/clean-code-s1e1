b34f9f258e18dfe564bbf4627e6424cfb4a25ea4
LidaChk <lvglazko@gmail.com>
2022-05-31 01:23:35 +0300

    refactor: 🎈 class name due 3.1. in  html-and-css.md

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("new-task__inp");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("task-list--incompleted");//ul of #task-list--incompleted
 var completedTasksHolder=document.getElementById("task-list--completed");//task-list--completed
